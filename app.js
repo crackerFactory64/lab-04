829fe4c98b284424ca9eb2fff2a1b5c9ce991c65
Lee Mander <lee.mander@gmail.com>
2023-09-14 11:00:31 +0100

    Refactor JS

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-const currentTime = new Date().getHours();
 let userPhone = prompt("What phone do you use?");
-let message = "User's phone: " + userPhone;
 
-document.write(message);
-
-if (userPhone == "Samsung" || userPhone == "samsung") {
-  alert("Snap!");
-} else {
-  alert("Oh...");
+function getUserPhone(){
+  if (userPhone == "Samsung" || userPhone == "samsung") {
+    alert("Snap!");
+  } else {
+    alert("Oh...");
+  }
 }
 
-if (currentTime < 12) {
-  document.write(" Good morning!");
-} else if (currentTime < 18) {
-  document.write(" Good afternoon!");
-} else {
-  document.write(" Good evening");
+getUserPhone();
+
+
+function displayUserPhone(){
+  let message = "User's phone: " + userPhone;
+  document.write(message);
 }
+
+
+function displayGreeting(){
+  const currentTime = new Date().getHours();
+  if (currentTime < 12) {
+    document.write(" Good morning!");
+  } else if (currentTime < 18) {
+    document.write(" Good afternoon!");
+  } else {
+    document.write(" Good evening");
+  }
+}
+
